089d50cdaf46b1e764935c10de67403f2eeabbd6
Mike Petrusha <petrushamike@gmail.com>
2023-11-12 23:03:32 +0300

    fix: add breackpoints to hero seciton

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
-    <section className='my-10 flex w-full max-w-[315px] border border-red-600 md:my-20'>
-      test
+    <section className='m-auto my-10 flex w-full max-w-[315px] border border-red-600 md:my-20 md:max-w-[690px] xl:max-w-[1050px]'>
+      testasdf
     </section>
   );
 };
